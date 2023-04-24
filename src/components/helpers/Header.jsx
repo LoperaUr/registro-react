fcd1044357b40615835f9a0ee011516d83b447cb
LoperaUr <maloperaur@cesde.net>
2023-04-24 12:00:21 -0500

    0.1

--- src/components/helpers/Header.jsx
+++ src/components/helpers/Header.jsx
@@ -1,11 +1,9 @@
 import { Link } from 'react-router-dom'
-import imgHome from '../../../public/img/home.png'
 
 const Header = () => {
   return (
     <header >
-      {/* Imagen que redireccione al Home */}
-      <img src={imgHome} id='imgHome'/>
+      
       <nav>
         <Link to={'/'}>Home</Link>
         <Link to={'/users'}>Users</Link>
--- src/components/layouts/Usuarios.jsx
+++ src/components/layouts/Usuarios.jsx
@@ -7,32 +7,34 @@ const Usuarios = () => {
 	const [email, setEmail] = useState('')
 
 	return (
-		<section>
+		<section className="content">
 			<Header />
-
-			<form action="">
-				<input value={nombre} onChange={(e) => setNombre(e.target.value)} type="text" />
-				<input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
-				<input value={email} onChange={(e) => setEmail(e.target.value)} type="text" />
-
-				<input type="button" value={'submit'} />
-			</form>
-			<table>
-				<thead>
-					<tr>
-						<td>Nombre</td>
-						<td>Correo</td>
-						<td>Password</td>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>{nombre}</td>
-						<td>{password}</td>
-						<td>{email}</td>
-					</tr>
-				</tbody>
-			</table>
+			<section className="form-container">
+				<form action="">
+					<input value={nombre} onChange={(e) => setNombre(e.target.value)} type="text" />
+					<input value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+					<input value={email} onChange={(e) => setEmail(e.target.value)} type="text" />
+					<input type="button" value={'submit'} />
+				</form>
+			</section>
+			<section className='card-container'>
+				<table>
+					<thead>
+						<tr>
+							<td>Nombre</td>
+							<td>Correo</td>
+							<td>Password</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>{nombre}</td>
+							<td>{password}</td>
+							<td>{email}</td>
+						</tr>
+					</tbody>
+				</table>
+			</section>
 		</section>
 	)
 }
