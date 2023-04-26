b842234bc340649ace9f673f0f90719c6f0f1a2a
Manuel Lopera <114296815+LoperaUr@users.noreply.github.com>
2023-04-26 11:42:40 -0500

    strucutre users ok

--- src/components/layouts/Usuarios.jsx
+++ src/components/layouts/Usuarios.jsx
@@ -12,41 +12,36 @@ const Usuarios = () => {
       <section className="content">
         <section className="form-container">
           <form action="">
+            <label htmlFor="">Nombre:</label>
             <input
               value={nombre}
               onChange={(e) => setNombre(e.target.value)}
               type="text"
             />
+            <label htmlFor="">PassWord:</label>
             <input
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               type="password"
             />
+            <label htmlFor="">Email:</label>
             <input
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               type="text"
             />
-            <input type="button" value={"submit"} />
+            <div className="button-submit">Enviar</div>
           </form>
         </section>
         <section className="card-container">
-          <table>
-            <thead>
-              <tr>
-                <td>Nombre</td>
-                <td>Correo</td>
-                <td>Password</td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{nombre}</td>
-                <td>{password}</td>
-                <td>{email}</td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="card">
+            <h3>Nombre: </h3>
+            <h4>{nombre}</h4>
+            <h3>Password: </h3>
+            <h4>{password}</h4>
+            <h3>Email: </h3>
+            <h4>{email}</h4>
+          </div>
         </section>
       </section>
     </section>
